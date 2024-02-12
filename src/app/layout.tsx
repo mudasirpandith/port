e4b46a75e1dcb07f891603b0f170ad186a680cf4
Mudasir Pandith <78873815+mudasirpandith@users.noreply.github.com>
2024-02-12 15:00:40 +0530

    v1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,20 +5,24 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  metadataBase: new URL("https://unkey.dev"),
-  title: "Open Source API Authentication",
-  description: "Accelerate your API development",
+  metadataBase: new URL("https://mudasirpandithlive.vercel.app/"),
+  title: "Mudasir Pandith",
+  description: "Full Stack Developer",
   openGraph: {
-    title: "Open Source API Authentication",
-    description: "Accelerate your API development ",
-    url: "https://unkey.dev",
+    title: "Mudasir Pandith",
+    description: "Full Stack Developer",
+    url: "https://mudasirpandithlive.vercel.app/",
     siteName: "unkey.dev",
-    images: ["https://unkey.dev/images/landing/og.png"],
+    images: [
+      "https://media.licdn.com/dms/image/D4D03AQHsaW5o5lzePg/profile-displayphoto-shrink_400_400/0/1687548042583?e=1713398400&v=beta&t=-ppZLAZWM6oUktsLsF9h7dQzubUP7yvPqS4EHoA-Mv8",
+    ],
   },
   twitter: {
     title: "Unkey",
     card: "summary_large_image",
-    images: ["https://unkey.dev/images/landing/og.png"],
+    images: [
+      "https://media.licdn.com/dms/image/D4D03AQHsaW5o5lzePg/profile-displayphoto-shrink_400_400/0/1687548042583?e=1713398400&v=beta&t=-ppZLAZWM6oUktsLsF9h7dQzubUP7yvPqS4EHoA-Mv8",
+    ],
   },
   robots: {
     index: true,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -10,27 +9,40 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex w-10 hover:first-letter:bg-red-50">
-        <AlertDialog>
-          <AlertDialogTrigger>Open</AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+    <main>
+      <div
+        className="bg-cover md:bg-center bg-no-repeat h-screen"
+        style={{
+          backgroundImage:
+            "url('https://img.freepik.com/free-vector/creative-geometric-design-space_53876-89787.jpg')",
+        }}
+      >
+        <div className="flex flex-col justify-center items-center h-full  animate-fadeIn">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-white text-center px-4 animate-slideUp delay-100">
+            Mudasir Pandith
+          </h1>
+          <p className="text-xl md:text-2xl lg:text-3xl text-white text-center mt-4 px-6 ">
+            Crafting unique digital experiences for the web.
+          </p>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="outline">Contact Me</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Coming soon</AlertDialogTitle>
+                <AlertDialogDescription>Working...</AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogAction>Close</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
       </div>
     </main>
   );
